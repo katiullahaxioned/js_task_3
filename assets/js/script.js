9cdc1c9456d6405a4c91cf0250845f80d0125ca4
katiullahaxioned <atiullahk@axioned.com>
2022-08-20 17:05:24 +0530

    value added dynamically in program

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,41 @@
-window.addEventListener("load", function () {
-  var waterAmount = document.querySelector(".water-amount");
-  var waterPercentage = document.querySelector(".water-percentage");
-  var waterQuantity = document.querySelector(".water-quantity");
-  var waterFilled = document.querySelector(".water-filled");
-  var bottle = document.querySelectorAll(".bottle");
+var waterAmount = document.querySelector(".water-amount");
+var waterPercentage = document.querySelector(".water-percentage");
+var waterQuantity = document.querySelector(".water-quantity");
+var waterFilled = document.querySelector(".water-filled");
+var waterBottle = document.querySelector(".water-bottle");
 
-  bottle.forEach(function (element, index) {
-    var percent = element.dataset.quantity;
-    var number = element.dataset.num;
+for (var k = 1; k < 8; k++) {
+  waterBottle.innerHTML += '<li class="bottle"><span>250</span><span>ml</span></li>';
+}
 
-    element.addEventListener("click", function () {
-      for (var i = 0; i < bottle.length; i++) {
-        bottle[i].classList.remove("bgColor");
-      }
+var bottle = document.querySelectorAll(".bottle");
 
-      var a = 2;
-      var arr = [];
-      waterAmount.innerHTML = a - number;
-      waterPercentage.innerHTML = percent + "%";
-      waterQuantity.style.height = 100 - percent + "%";
-      waterFilled.style.height = percent + "%";
+var waterQuantityHeight = waterQuantity.clientHeight;
+var waterFilledHeight = waterFilled.clientHeight;
 
-      if (arr.indexOf(index) === -1) {
-        arr.push(index);
-      }
+bottle.forEach(function (element, index) {
+  element.addEventListener("click", function () {
+    var totalLiter = 2;
+    var totalPercent = 100;
+    var initialHeight = waterQuantityHeight / bottle.length * (index + 1);
+    var initialPercent = (totalPercent / bottle.length) * (index + 1);
+    var initialFill = (initialPercent * totalLiter) / totalPercent;
 
-      for (var i = 0; i <= arr[0]; i++) {
-        bottle[i].classList.add("bgColor");
-      }
-    });
+    waterAmount.innerHTML = totalLiter - initialFill;
+    waterPercentage.innerHTML = initialPercent + "%";
+    waterQuantity.style.height = (waterQuantityHeight - initialHeight) + "px";
+    waterFilled.style.height = initialHeight + "px";
+
+    for (var i = 0; i < bottle.length; i++) {
+      bottle[i].classList.remove("bgColor");
+    }
+
+    var arr = [];
+    if (arr.indexOf(index) === -1) {
+      arr.push(index);
+    }
+    for (var i = 0; i <= arr[0]; i++) {
+      bottle[i].classList.add("bgColor");
+    }
   });
 });
